c1d815f2019b7e8ceef575f91081255cb14dffff
BorisArco <boris.arco@gmail.com>
2022-04-27 11:03:25 -0400

    Add img,table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,5 @@ class PostsController < ApplicationController
       content: params[:content],
       image_url: params[:image_url])
    
-   #  if @post.save
-      # redirect_to root_path
-    # end
   end
 end
