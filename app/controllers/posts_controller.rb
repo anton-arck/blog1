2efacff7bb1b6de5adb6f1d0c381dffabcc13072
BorisArco <boris.arco@gmail.com>
2022-04-27 00:41:37 -0400

    add cdn bootstrap,set root page,form

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def create
+    @post = Post.create(
+      title: params[:title],
+      content: params[:content],
+      image_url: params[:image_url])
+   
+    if @post.save
+      redirect_to root_path
+    end
+  end
+end
